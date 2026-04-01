cab31e590d84f973abd06dd27ebe40eef5836531
yara22aulia <yara22aulia@gmail.com>
2026-04-01 11:02:16 +0700

    first commit

---
+++ logika or.cpp
@@ -0,0 +1,22 @@
+Logika OR
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = " <<status<< ", dengan nilai rata-rata = " << rerata << endl;
+}
