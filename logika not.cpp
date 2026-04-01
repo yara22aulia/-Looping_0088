c9e976400ff27c2716219637419993808ec4223b
yara22aulia <yara22aulia@gmail.com>
2026-04-01 10:58:38 +0700

    first commit

---
+++ logika not.cpp
@@ -0,0 +1,22 @@
+Logika not
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if(!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+    
+    cout<<"Status kelulusan = " <<status<<" ,dengan nilai rata-rata = " << rerata << endl;
+}
